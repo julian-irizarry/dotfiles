1799adf57f2f1e3de323e78da0e690c1cbba7a25
irizajc1 <julian.irizarry@jhuapl.edu>
2023-07-17 19:41:33 -0400

    added harpoon and fixed keymaps

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -4,21 +4,38 @@ local Plugins = {
   {'tpope/vim-repeat'},
   {'christoomey/vim-tmux-navigator', lazy = false},
   {'kyazdani42/nvim-web-devicons', lazy = true},
-	{'mg979/vim-visual-multi'},
-	{'vimpostor/vim-tpipeline', lazy=false},
+  {'mg979/vim-visual-multi'},
+  {'vimpostor/vim-tpipeline', lazy=false},
   {'numToStr/Comment.nvim', config = true, event = 'VeryLazy', 
     init = function ()
       vim.keymap.set('n', '<leader>/', function() require("Comment.api").toggle.linewise.current() end)
       vim.keymap.set('v', '<leader>/', "<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>")
     end
   },
+  {'ThePrimeagen/harpoon',
+  	init = function ()
+		local mark = require('harpoon.mark')
+		local ui = require('harpoon.ui')
+
+		vim.keymap.set('n', '<leader>a', mark.add_file)
+		vim.keymap.set('n', '<C-e>', ui.toggle_quick_menu)
+
+		vim.keymap.set('n', '<C-p>', function() ui.nav_next() end)
+		vim.keymap.set('n', '<C-g>', function() ui.nav_prev() end)
+	end
+  },
   
   -- Themes
   {'notken12/base46-colors'},
+  {'dasupradyumna/midnight.nvim', lazy = false, priority = 1000},
+  {'decaycs/decay.nvim'},
+  {'projekt0n/github-nvim-theme'},
   {'folke/tokyonight.nvim'},
   {'joshdick/onedark.vim'},
   {'tanvirtin/monokai.nvim'},
-  {'lunarvim/darkplus.nvim'},
+  {'challenger-deep-theme/vim'},
+  -- {'lunarvim/darkplus.nvim'},
+  {'martinsione/darkplus.nvim'},
 }
 
 return Plugins
